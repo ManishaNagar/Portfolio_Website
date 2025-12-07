3a970edc97df7f08a681c35d314b89847662a309
ManishaNagar <manishanagar2641@gmail.com>
2025-12-08 01:18:52 +0530

    Add files via upload

---
+++ Responsive portfolio/script.js
@@ -0,0 +1,45 @@
+// ================= NAVBAR TOGGLE =================
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+// toggle menu open/close
+menuIcon.onclick = () => {
+    const isOpen = navbar.classList.toggle("active");
+
+    if (isOpen) {
+        //show cross icon
+        menuIcon.classList.remove("bx-menu");
+        menuIcon.classList.add("bx-x");
+    } else {
+        // shows three horizontal lines icon
+        menuIcon.classList.remove("bx-x");
+        menuIcon.classList.add("bx-menu");
+    }
+};
+
+// close menu when clicking a link
+document.querySelectorAll(".navbar a").forEach(link => {
+    link.onclick = () => {
+        // close menu when clicking a link  that is when a navigation link is clicked and shows cross icon
+        navbar.classList.remove("active");
+        menuIcon.classList.remove("bx-x");
+        menuIcon.classList.add("bx-menu");
+    };
+});
+
+// ================= PHONE NUMBER VALIDATION =================
+document.getElementById("phone").addEventListener("input", function () {
+    let phone = this.value;
+    let err = document.getElementById("phone-error");
+
+    if (/^[0-9]{10}$/.test(phone)) {
+        err.style.display = "none";
+    } else {
+        err.style.display = "block";
+    }
+});
+
+
+//Add DB
+//UPDATE completed  
